85f161982f515e47a86c46910999836c27efb2e5
AbdoMossed <abdomossedmoaet2004@gmail.com>
2023-12-07 15:18:09 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('gold:refresh')->everyFifteenMinutes()->withoutOverlapping();
+        $schedule->command('currencies:refresh')->everyFifteenMinutes()->withoutOverlapping();
     }
 
     /**
